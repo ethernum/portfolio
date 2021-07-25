a5b8f3745cb2185e3635fd61d541cf477f743c4c
Sébastien Charron <seblarichaud@gmail.com>
2021-07-25 12:30:18 +0200

    First Commit

---
+++ src/ResumeData.js
@@ -0,0 +1,93 @@
+let resumeData = {
+  imagebaseurl: "",
+  name: "Sébastien Charron",
+  role: "Dev Fullstack (bon, ok plutôt Front quand même)  ",
+  roleDescription:
+    "J'aime créer des applications et des sites soignés, avec un code propre, et cela en apprenant constamment.",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/s%C3%A9bastien-charron-07709b218/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "https://github.com/ethernum",
+      className: "fa fa-github",
+    },
+  ],
+  aboutme:
+    "Salut la compagnie! Je m'appelle Sébastien, description rapide :  je suis en recherche de mission freelance. J'aime me documenter sur les nouveaux languages, framework, paradigme. Je suis en poste actuellement en cdi mais je souhaite voir autre chose. etc...",
+  address: "France, Paris",
+  adressMail: "sebastien.charron9@gmail.com",
+  education: [
+    {
+      UniversityName: "Aforp Issy les Moulineaux",
+      specialization: "BTS SIO",
+      MonthOfPassing: "Août",
+      YearOfPassing: "2017",
+      Achievements: "",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "Cegedim",
+      specialization: "Chef De Projet Technique",
+      MonthOfLeaving: "Poste",
+      YearOfLeaving: " Actuel",
+      Achievements: "",
+    },
+    {
+      CompanyName: "Hemmersbach",
+      specialization: "Technicien Informatique",
+      MonthOfLeaving: "Décembre",
+      YearOfLeaving: "2019",
+      Achievements: "",
+    },
+  ],
+  skillsDescription: "Your skills here",
+  skills: [
+    {
+      skillname: "Express",
+    },
+    {
+      skillname: "GitHub",
+    },
+    {
+      skillname: "anglais",
+    },
+    {
+      skillname: "Javascript",
+    },
+    {
+      skillname: "Reactjs",
+    },
+    {
+      skillname: "nextJS",
+    },
+  ],
+  comet: "   pouet",
+  portfolio: [
+    {
+      name: "project1",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+    {
+      name: "project2",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project.jpg",
+    },
+    {
+      name: "project3",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project2.jpg",
+    },
+    {
+      name: "project4",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+  ]
+};
+export default resumeData;
